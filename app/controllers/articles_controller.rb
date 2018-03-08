4ea33752703a1582714b82db4151dde8944af811
tochman <thomas@craftacademy.se>
2018-03-08 23:00:42 +0100

    SPIKE first attempt at Active Storage

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    article = Article.create(article_params)
+    article.image.attach(params[:article][:image])
+    redirect_to article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
