d4d3c279c7563792ffb1ecdcf51733e4de329725
oliver_ochman <oliver.ochman@hotmail.com>
2018-03-22 15:31:34 +0100

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :articles, only: [:new, :create, :show, :index]
 
   root controller: :articles, action: :index
+
+  namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
 end
