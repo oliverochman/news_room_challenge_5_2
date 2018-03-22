9909844833c9a5994e7f908c34a0bea84ef8fefd
oliver_ochman <oliver.ochman@hotmail.com>
2018-03-22 15:32:43 +0100

    Update User DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_20_183749) do
+ActiveRecord::Schema.define(version: 2018_03_22_111712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,8 +60,12 @@ ActiveRecord::Schema.define(version: 2018_03_20_183749) do
     t.float "latitude"
     t.float "longitude"
     t.text "address"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.text "tokens"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
