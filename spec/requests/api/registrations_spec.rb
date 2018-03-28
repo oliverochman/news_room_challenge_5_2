36f35781781bccb90f4a0ea998f4903c42bf16ab
oliver_ochman <oliver.ochman@hotmail.com>
2018-03-28 10:10:58 +0200

    Add omniauth request spec

--- spec/requests/api/registrations_spec.rb
+++ spec/requests/api/registrations_spec.rb
@@ -50,4 +50,27 @@ RSpec.describe 'User Registration', type: :request do
       expect(response.status).to eq 422
     end
   end
+
+  describe 'OmniAuth' do
+    context 'Facebook' do
+
+      let(:params) { {omniauth_window_type: 'newWindow'} }
+      let(:request) { lambda do
+        get('/api/v1/auth/facebook/',
+            params: params,
+            headers: headers)
+        follow_redirect! until response.status == 200
+      end }
+
+      it 'allows user to register with valid authorization' do
+        # Uses default mock, set in support/oauth.rb
+        expect { request.call }.to change(User, :count).by(1)
+      end
+
+      it 'fails to register user with invalid authorization' do
+        OmniAuth.config.mock_auth[:facebook] = :invalid_credentials
+        expect { request.call }.to change(User, :count).by(0)
+      end
+    end
+  end
 end
