3d70d8320a6f233c6656c83c3521dff5e074d845
oliver_ochman <oliver.ochman@hotmail.com>
2018-04-24 11:31:39 +0200

    fix access problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
